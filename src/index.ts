32afe1e84e528ba6e7bcbdda4f93df2ec1d6e748
myko <awesomeme155@gmail.com>
2021-04-09 03:34:53 +0300

    feat: setup session state with redis
    - install and configure redis
    - install and configure express-session

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,46 @@
-import express from 'express';
+import express, { Router, Response } from 'express';
 import { createServer } from 'http';
+import session from 'express-session';
+import connectRedis from 'connect-redis';
+import Redis from 'ioredis';
+
+require('dotenv').config();
 
 const app = express();
 const server = createServer(app);
-server.listen({ port: 8000}, () => console.log('Yai, our server is up and running'));
+const router = Router();
+
+const RedisStoreWithSession = connectRedis(session);
+const redis = new Redis({
+  host: process.env.REDIS_HOST,
+  port: Number(process.env.REDIS_PORT),
+});
+const redisStoreInstance = new RedisStoreWithSession({ client: redis });
+
+app.use(session({
+  store: redisStoreInstance,
+  name: process.env.COOKIE_NAME,
+  secret: process.env.SESSION_SECRET as string,
+  resave: false,
+  saveUninitialized: false,
+  cookie: {
+    path: '/',
+    httpOnly: true,
+    secure: false,
+    maxAge: 1000 * 60 * 60 * 24,
+  },
+}));
+app.use(router);
+
+router.get('/', (req: any, res: Response) => {
+  if (!req.session!.userId) {
+    req.session.userId = req.query.userId;
+    req.session.loadedCount = 0;
+  } else {
+      req.session!.loadedCount = Number(req.session.loadedCount) + 1;
+  }
+
+  res.send(`userId: ${req.session!.userId}, loadedCount: ${req.session!.loadedCount}`);
+});
+
+server.listen({ port: process.env.SERVER_PORT }, () => console.log(`Yai, our server is up and running on port ${process.env.SERVER_PORT}`));
