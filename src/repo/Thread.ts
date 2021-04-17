ac896a3aa9bd6bdf47b9b3daa270338aa29c5a4c
myko <awesomeme155@gmail.com>
2021-04-18 02:55:35 +0300

    feat: add thread entities - add thread entity - add threadItem entity - add relationships between user, thread and thread items

---
+++ src/repo/Thread.ts
@@ -0,0 +1,37 @@
+import {
+  PrimaryGeneratedColumn,
+  Column,
+  Entity,
+  ManyToOne,
+  OneToMany,
+} from 'typeorm';
+import { Length } from 'class-validator';
+
+import { User } from './User';
+import { ThreadItem } from './ThreadItem';
+
+@Entity({ name: 'Threads' })
+export class Thread {
+    @PrimaryGeneratedColumn({ name: 'Id', type: 'bigint' })
+    id!: string
+
+    @Column('int', { name: 'Views', default: 0, nullable: false })
+    views!: number
+
+    @Column('boolean', { name: 'isDisabled', default: false, nullable: false })
+    isDisabled!: boolean
+
+    @Column('varchar', { name: 'Title', length: 150, nullable: false })
+    @Length(5, 10)
+    name!: string
+
+    @Column('varchar', { name: 'Body', length: 2500, nullable: false })
+    @Length(10, 2500)
+    body!: string
+
+    @ManyToOne(() => User, (user) => user.threads)
+    user!: User
+
+    @OneToMany(() => ThreadItem, (threadItems) => threadItems.thread)
+    threadItems!: ThreadItem[]
+}
